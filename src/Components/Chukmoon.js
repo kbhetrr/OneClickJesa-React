be94222f300a28fa2cadd39a705a6e920f965f47
KyoungHwan Kim <kimkh7534@naver.com>
2021-08-08 10:32:14 +0900

    네비게이션 바 수정

--- src/Components/Chukmoon.js
+++ src/Components/Chukmoon.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 const DATA = [
   "향근현현현 유",
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -5,10 +5,10 @@ const Navigation = () => {
   return (
     <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
       <Container>
+        <Navbar.Brand href="/" className="menu-title">원클릭 제사</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <Nav.Link href="/">홈</Nav.Link>
+          <Nav className="ml-auto">
             <Nav.Link href="/write">축문 & 지방 작성</Nav.Link>
             <Nav.Link href="/info">제사 관련 정보</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>

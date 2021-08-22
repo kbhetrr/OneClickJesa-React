512061378f0473f70565f1e0b08459930ee8025d
KyoungHwan Kim <kimkh7534@naver.com>
2021-08-23 02:22:20 +0900

    음력 파싱 함수 수정

--- src/pages/Write/getLunarInfo.js
+++ src/pages/Write/getLunarInfo.js
@@ -7,10 +7,10 @@ const getLunarInfo = (year, month, day) => {
     .then((res) => {
       const data = res.data;
       console.log(
-        `양력날짜 : ${data.SOLC_YYYY}년 ${data.SOLC_MM}월 ${data.SOLC_DD}일`
+        `양력날짜 ${data.SOLC_YYYY}년 ${data.SOLC_MM}월 ${data.SOLC_DD}일`
       );
       console.log(
-        `음력날짜 : ${data.LUNC_YYYY}년 ${data.LUNC_MM}월 ${data.LUNC_DD}일`
+        `음력날짜 ${data.LUNC_YYYY}년 ${data.LUNC_MM}월 ${data.LUNC_DD}일`
       );
       console.log(res);
     })

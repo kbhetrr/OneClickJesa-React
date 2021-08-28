27590c5fe4a52080f9076e14886478c27f011679
KyoungHwan Kim <kimkh7534@naver.com>
2021-08-28 17:31:31 +0900

    홈 화면 수정

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer
+      style={{
+        backgroundColor: "#c5cde4",
+        paddingTop: "7%",
+        paddingBottom: "5%",
+      }}
+    >
       <h6 className="text-center">Copyright 2021. KKH all rights reserved.</h6>
     </footer>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,16 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1
-        className="text-center"
-        style={{ paddingTop: "30%", paddingBottom: "30%" }}
-      >
-        Home
-      </h1>
+    <div
+      className="container"
+      style={{
+        paddingTop: "5%",
+        paddingBottom: "5%",
+        backgroundColor: "#e5e4e9",
+      }}
+    >
+      <h1>원클릭 제사</h1>
+      <h4>클릭 몇 번으로 제사 준비를 완료하세요!</h4>
     </div>
   );
 };

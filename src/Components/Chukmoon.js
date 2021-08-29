b743204baf5cd1784ac850d940433900874b00c4
KyoungHwan Kim <kimkh7534@naver.com>
2021-08-29 16:00:32 +0900

    축문 화면 수정

--- src/Components/Chukmoon.js
+++ src/Components/Chukmoon.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Button, Container } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 const DATA = [
   "향근현현현 유",
@@ -26,7 +26,14 @@ const DATA = [
   "     우ㅇ",
 ];
 
-const DATA_1 = ["유 세차 ㅇㅇ ㅇ월 ㅇㅇ 삭 ㅇㅇ일 ㅇㅇ", ""];
+const DATA_ = [
+  "유 세차 ㅇㅇ ㅇㅇ월 ㅇㅇ 삭 ㅇㅇ일 ㅇㅇ",
+  "효자 ㅇㅇ 감소고우",
+  "현고학생부군 세서천역",
+  "휘일부림 추원감시 호천망극",
+  "근이 청작서수 공신전헌 상",
+  "향",
+];
 
 let idxRow = 0;
 let idxCol = 0;
@@ -37,7 +44,7 @@ const Chukmoon = (props) => {
       className="border border-dark"
       xs={11}
       sm={11}
-      md={4}
+      md={6}
       style={{
         paddingTop: 32,
         paddingBottom: 32,
@@ -64,7 +71,22 @@ const Chukmoon = (props) => {
           </Row>
         );
       })}
-      <Row>{props.Hang1}</Row>
+      {/*DATA_.reverse().map((s) => {
+        idxRow += 1;
+        return (
+          <Col key={idxRow}>
+            {s.split("").map((text) => {
+              console.log(text);
+              idxCol += 1;
+              return (
+                <Row key={idxCol} style={{ textAlign: "center", width: 1 }}>
+                  {text}
+                </Row>
+              );
+            })}
+          </Col>
+        );
+      })*/}
     </Col>
   );
 };

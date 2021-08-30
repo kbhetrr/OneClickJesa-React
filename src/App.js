ecfae035a0e88ad73de6ae3993ff3c97706e7e12
KyoungHwan Kim <kimkh7534@naver.com>
2021-08-30 20:51:52 +0900

    패키지 수정

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from "./Components/Footer";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 const App = () => {
   return (
